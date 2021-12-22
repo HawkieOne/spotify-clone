44dbbdb20f774bd280f22f781a302a392462b611
Håkan <hali0151@student.umu.se>
2021-12-23 00:20:36 +0100

    initial commit

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null,
+});
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: "601WLbJ3Vj91XIugGUJNUe",
+});
